e890c5ab15df29ce664681c02344cfd71c7f9055
Dariusz Okoński <darek200180@gmail.com>
2024-04-21 13:58:08 +0200

    work on hero

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <div>
-      <h2>Hero</h2>
+    <div className='hero-container'>
+      <div className='hero-center'>
+        <h1>
+          Manage Any Content <br /> Anywhere
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+          adipisci et reiciendis sit voluptate officia voluptatem at ab ut
+          animi?
+        </p>
+      </div>
     </div>
   );
 };
