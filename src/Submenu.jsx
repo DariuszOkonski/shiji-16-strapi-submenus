5a932296ac18e68cfd43b623bd1d9722bda9a540
Dariusz Okoński <darek200180@gmail.com>
2024-04-21 14:46:46 +0200

    add submienu

--- src/Submenu.jsx
+++ src/Submenu.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import { useGlobalContext } from './context';
+import sublinks from './data';
 
 const Submenu = () => {
+  const { pageId, setPageId } = useGlobalContext();
+  const currentPage = sublinks.find((item) => item.pageId === pageId);
+
   return (
-    <div>
-      <h2>Submenu</h2>
+    <div className='submenu'>
+      <h5>{currentPage?.page}</h5>
+      <div
+        className='submenu-links'
+        style={{
+          gridTemplateColumns:
+            currentPage?.links?.length > 3 ? '1fr 1fr' : '1fr',
+        }}
+      >
+        {currentPage?.links?.map((link) => {
+          const { id, url, label, icon } = link;
+          return (
+            <a key={id} href={url}>
+              {icon} {label}
+            </a>
+          );
+        })}
+      </div>
     </div>
   );
 };
