5ff73875d7ea1f685455f0f0229ef85190389482
Dariusz Okoński <darek200180@gmail.com>
2024-04-21 13:10:23 +0200

    add context

---
+++ src/Context.jsx
@@ -0,0 +1,29 @@
+import { createContext, useContext, useState } from 'react';
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  return (
+    <AppContext.Provider value={{ isSidebarOpen, openSidebar, closeSidebar }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  const context = useContext(AppContext);
+  if (!context) {
+    throw new Error('Context is out of Provider');
+  }
+  return context;
+};
